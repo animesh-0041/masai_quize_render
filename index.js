199cd0c584135b4c0dc65c5f5868d7c3254a6a76
animesh-0041 <ghoroianimesh1508@gmail.com>
2023-07-10 15:16:06 +0530

    backend

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server"); // importing json-server library
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = 8080; // you can use any port number here; i chose to use 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
